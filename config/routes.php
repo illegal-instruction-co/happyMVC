40c0e466d6ba468d982ec9b39e53201855771557
illegal-instruction-co <machinetherapist2@gmail.com>
2023-03-20 21:12:37 +0300

    arrange routes as more understable

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,5 @@
 <?php
 # An example about how can be happy :)
 setRoute("example", "example@indexAction"); // First is homepage
-setRoute("example/try-your-message/{msg}", "example@tryYourMessage");
-setRoute("deneme", function() { echo"addasda"; }); 
+setRoute("example/with-parameter/{param}", "example@withParameter");
+setRoute("lambda", function() { echo"LAMBDA"; }); 
