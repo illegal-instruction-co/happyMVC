37131da123676e939a46fb47fce2a70cedd0391d
illegal instruction <79029454+illegal-instruction-co@users.noreply.github.com>
2023-03-20 21:00:19 +0300

    update routes

--- config/routes.php
+++ config/routes.php
@@ -2,4 +2,4 @@
 # An example about how can be happy :)
 setRoute("example", "example@indexAction"); // First is homepage
 setRoute("example/try-your-message/{msg}", "example@tryYourMessage");
-setRoute("deneme", function() { echo"addasda"; }); 
+setRoute("lambda", function() { echo "Lambda"; }); 
